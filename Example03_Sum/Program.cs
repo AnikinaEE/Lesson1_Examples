5cbec81ac18aa5808ae60db99937d9c582355971
Elena Anikina <faniya@bk.ru>
2023-02-11 20:00:51 +0300

    add Random

--- Example03_Sum/Program.cs
+++ Example03_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 6;
-int numberB = 9;
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.Write("Сумма: ");
 Console.WriteLine(result);
