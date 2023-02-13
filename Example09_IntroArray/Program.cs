4f317a00d297d442333b1e2c04aa53867f521438
Elena Anikina <faniya@bk.ru>
2023-02-13 16:23:05 +0300

    Example09 Intro Array

---
+++ Example09_IntroArray/Program.cs
@@ -0,0 +1,23 @@
+﻿// Max - функция определения максимума 
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+// Объявляем массив
+int[] array = {11,21,31,41,15,61,17,18,19};
+
+// array[0] - обрвщение к элементу массива с индексом 0
+// Console.WriteLine(array[0]);
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+Console.Write("Максимальное значение равно: " );
+Console.WriteLine(max);
