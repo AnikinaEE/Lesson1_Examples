eef17eaba4cbb5aced437c41c64fd89694a33566
Elena Anikina <faniya@bk.ru>
2023-02-13 16:40:30 +0300

    Example10 Method Array

---
+++ Example10_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1,12,31,4,18,15,16,17,18};
+
+// Lentch - команда, которая запрашивает длинну массива (кол-во элементов)
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+   if (array[index] == find)
+   {
+       Console.WriteLine(index); 
+       break; // прерывание цикла, в случае обнаружения элемента равного find
+   } 
+   // index = index + 1;
+   index++; 
+}
